d382b0ffae58f5873dab2329396fec44788fb694
Eva Pace <eba.pachi@gmail.com>
2023-11-06 18:11:45 -0300

    js: make image component work (change btns etc)

--- js/base/index.js
+++ js/base/index.js
@@ -1,9 +1,10 @@
 import button from './button.js'
 import h1 from './h1.js'
 import img from './img.js'
+import p from './p.js'
 
 const createElement = tag => document.createElement(tag)
 
 const div = () => createElement('div')
 
-export { button, h1, img, div }
+export { button, h1, img, div, p }
--- js/components/Image.js
+++ js/components/Image.js
@@ -1,36 +1,61 @@
-import { button, h1, img } from '../base/index.js'
+import { button, h1, img, p } from '../base/index.js'
 import srcTree from '../src-tree.js'
 
-console.log('srcTree', srcTree)
-
-const Image = (src, outerOnClick) => {
+const Image = initialSrc => {
     const outer = document.createElement('div')
-    outer.append(h1(src))
+    outer.append(h1(initialSrc))
 
-    const image = img(`../assets/${src}.jpg`)
+    const image = img(`../assets/${initialSrc}.jpg`)
     outer.append(image)
 
-    let btnA, btnB
+    let inner, btnA, btnB
 
-    const onClick = newSrc => function onClick (_event) {
-        const [nextA, nextB] = srcTree[newSrc].next
-        btnA.innerText = srcTree[nextA].title
-        btnB.innerText = srcTree[nextB].title
+    const onClick = function onClick (_event) {
+        const url = new URL(image.src)
+        const pathname = url.pathname
+        const imgName = pathname // '/assets/initial.jpg'
+            .split('/') // ['', 'assets', 'initial.jpg']
+            .pop() // initial.jpg
+            .split('.') // ['initial', 'jpg']
+            .shift() // 'initial'
+
+        const [optionA, optionB] = srcTree[imgName].next
+        let newSrc
+        if (this.id == 'btnA') {
+            newSrc = optionA
+        } else if (this.id == 'btnB') {
+            newSrc = optionB
+        }
 
         image.src = `../assets/${newSrc}.jpg`
+
+        if (srcTree[newSrc].next) {
+            const [nextA, nextB] = srcTree[newSrc].next
+            btnA.innerText = srcTree[nextA].title
+            btnB.innerText = srcTree[nextB].title
+        } else {
+            if (inner.hasChildNodes()) {
+                inner.removeChild(btnA)
+                inner.removeChild(btnB)
+                outer.removeChild(inner)
+                outer.append(p('acabou a história!'))
+            }
+        }
     }
 
-    const [optionA, optionB] = srcTree[src].next
+    const [optionA, optionB] = srcTree[initialSrc].next
 
     btnA = button(srcTree[optionA].title)
-    btnB = button(srcTree[optionB].title)
+    btnA.id = 'btnA'
+    btnA.onclick = onClick
 
-    const inner = document.createElement('div')
+    btnB = button(srcTree[optionB].title)
+    btnB.id = 'btnB'
+    btnB.onclick = onClick
+
+    inner = document.createElement('div')
     inner.append(btnA)
     inner.append(btnB)
-
-    btnA.onclick = onClick(optionA)
-    btnB.onclick = onClick(optionB)
 
     outer.append(inner)
     return outer
