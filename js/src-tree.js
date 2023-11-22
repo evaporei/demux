379e256d11ef0b0f411952d5b9f6d937b71598ac
Eva Pace <eba.pachi@gmail.com>
2023-11-22 18:34:02 -0300

    js: remove https://youtu.be/ prefix from URLs

--- js/src-tree.js
+++ js/src-tree.js
@@ -2,33 +2,33 @@ const srcTree = {
     "initial": {
         title: "Intro",
         next: ["ruby01", "emerald01"],
-        url: 'https://youtu.be/HuBS3PF0jNI',
+        url: 'HuBS3PF0jNI',
     },
     "ruby01": {
         title: "Rubi",
         next: ["ruby02", "ruby03"],
-        url: 'https://youtu.be/3gg14C-Vsp4',
+        url: '3gg14C-Vsp4',
     },
     "emerald01": {
         title: "Isabella",
         next: ["emerald02", "emerald03"],
-        url: 'https://youtu.be/m8_jhFFa-6I',
+        url: 'm8_jhFFa-6I',
     },
     "ruby02": {
         title: "Fugir",
-        url: 'https://youtu.be/m7o5R5uC02c',
+        url: 'm7o5R5uC02c',
     },
     "ruby03": {
         title: "Lutar",
-        url: 'https://youtu.be/w6zf1NBeM0U',
+        url: 'w6zf1NBeM0U',
     },
     "emerald02": {
         title: "Encontro",
-        url: ' https://youtu.be/RJBoWKiC-nA',
+        url: 'RJBoWKiC-nA',
     },
     "emerald03": {
         title: "Conhecer melhor",
-        url: 'https://youtu.be/nco1MFlZNMQ',
+        url: 'nco1MFlZNMQ',
     },
 }
 
