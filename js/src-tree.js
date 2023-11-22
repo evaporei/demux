4d6b2f47771b7d00395b3734e9c55607dd5131b9
Eva Pace <eba.pachi@gmail.com>
2023-11-22 18:30:30 -0300

    js: add temporary video URLs

--- js/src-tree.js
+++ js/src-tree.js
@@ -2,26 +2,33 @@ const srcTree = {
     "initial": {
         title: "Intro",
         next: ["ruby01", "emerald01"],
+        url: 'https://youtu.be/HuBS3PF0jNI',
     },
     "ruby01": {
         title: "Rubi",
         next: ["ruby02", "ruby03"],
+        url: 'https://youtu.be/3gg14C-Vsp4',
     },
     "emerald01": {
         title: "Isabella",
         next: ["emerald02", "emerald03"],
+        url: 'https://youtu.be/m8_jhFFa-6I',
     },
     "ruby02": {
         title: "Fugir",
+        url: 'https://youtu.be/m7o5R5uC02c',
     },
     "ruby03": {
         title: "Lutar",
+        url: 'https://youtu.be/w6zf1NBeM0U',
     },
     "emerald02": {
         title: "Encontro",
+        url: ' https://youtu.be/RJBoWKiC-nA',
     },
     "emerald03": {
         title: "Conhecer melhor",
+        url: 'https://youtu.be/nco1MFlZNMQ',
     },
 }
 
