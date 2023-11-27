e0d5e9c0e3134243ef522834ffb1db624cf21f5a
Eva Pace <eba.pachi@gmail.com>
2023-11-27 11:56:57 -0300

    index: add window for content & update summary text

--- js/App.js
+++ js/App.js
@@ -3,31 +3,33 @@ import Video from './components/Video.js'
 
 async function App() {
     const d = div()
+    d.className = "window"
+    d.style = 'width: 70%'
 
-    let title = h1("Título")
-    title.className = "flex justify-center text-white py-4 text-xl"
-    d.append(title)
+    let titleBar = div()
+    titleBar.className = "title-bar"
+    let title = h1("SINOPSE")
+    title.className = "flex justify-center text-black py-4 text-xl title"
+    titleBar.append(title)
+    d.append(titleBar)
 
-    let summary1 = p('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
-    summary1.className = "flex justify-center text-white"
-    d.append(summary1)
-    let summary2 = p('Etiam a dignissim mauris.')
-    summary2.className = "flex justify-center text-white"
-    d.append(summary2)
-    let summary3 = p('Donec in elit vel erat pharetra aliquam vel eget ex.')
-    summary3.className = "flex justify-center text-white"
-    d.append(summary3)
-    let summary4 = p('Interdum et malesuada fames ac ante ipsum primis in faucibus.')
-    summary4.className = "flex justify-center text-white"
-    d.append(summary4)
-    let exemplo = p('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
-    exemplo.className = "flex justify-center text-black"
-    d.append(exemplo)
+    let separator = div()
+    separator.className = "separator"
+    d.append(separator)
+
+    let c = div() // content
+    c.className = "window-pane"
+
+    let summary = p('\xa0\xa0\xa0\xa0\xa0\xa0\xa0Após ser convidado por uma colega da faculdade para uma festa universitária, Lucas se encontra em um dilema. Com suas poucas habilidades sociais, ele deverá escolher em qual das duas garotas ele deverá investir: a garota popular e divertida que o convidou, ou a garota misteriosa e atraente que ele conheceu na festa. Quem Lucas deverá escolher? E como ele vai agir para conquistar a garota de seus sonhos?')
+    summary.className = "flex justify-center text-black"
+    c.append(summary)
 
     let curr = 'initial'
     let { outer, inner } = Video(curr)
-    d.append(outer)
-    d.append(inner)
+    c.append(outer)
+    c.append(inner)
+
+    d.append(c)
 
     return d
 }
--- js/base/button.js
+++ js/base/button.js
@@ -1,7 +1,8 @@
 const button = txt => {
     const btn = document.createElement('button')
     btn.innerText = txt
-    btn.className = 'nes-btn is-primary'
+    // btn.className = 'nes-btn is-primary'
+    btn.className = 'btn'
     return btn
 }
 
