1e6035a0bb09c03786fdbb62ddade47c7fbf175f
Eva Pace <eba.pachi@gmail.com>
2023-11-27 18:20:43 -0300

    about: revert js header, let's copy the navbar...

--- js/about.js
+++
@@ -1,9 +0,0 @@
-import Navbar from './components/Navbar.js'
-import HomeLogo from './components/HomeLogo.js'
-
-const app = () => {
-    document.body.append(HomeLogo())
-    document.body.append(Navbar())
-}
-
-app()
--- js/index.js
+++ js/index.js
@@ -1,14 +1,9 @@
 import App from './App.js'
-import Navbar from './components/Navbar.js'
-import HomeLogo from './components/HomeLogo.js'
 
 const app = () => {
     const app = document.getElementById('app')
-    const homeLogo = HomeLogo()
 
     app.append(App())
-    app.before(homeLogo)
-    homeLogo.after(Navbar())
 }
 
 app()
