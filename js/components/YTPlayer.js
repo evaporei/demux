b8c3f04b2d1050077c3d6f237805c47cd906fe16
Eva Pace <eba.pachi@gmail.com>
2023-11-27 16:36:45 -0300

    ytplayer: make 360p -> 720p

--- js/components/YTPlayer.js
+++ js/components/YTPlayer.js
@@ -2,8 +2,8 @@ const YTPlayer = videoId => {
     const player = document.createElement('iframe')
     player.id = 'ytplayer'
     player.setAttribute('type', 'text/html')
-    player.width = 640
-    player.height = 360
+    player.width = 1280
+    player.height = 720
     player.src = `https://www.youtube.com/embed/${videoId}?autoplay=0&origin=${location.hostname}`
     player.frameBorder = '0'
 
