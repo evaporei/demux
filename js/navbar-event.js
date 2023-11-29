51f946477bd34dc596d3cab10a132c06b1d1c773
Eva Pace <eba.pachi@gmail.com>
2023-11-29 14:10:07 -0300

    all: add home link

--- js/navbar-event.js
+++ js/navbar-event.js
@@ -1,6 +1,11 @@
+const homeLink = document.getElementById('home-link')
 const aboutLink = document.getElementById('about-link')
 const charsLink = document.getElementById('characters-link')
 const videosLink = document.getElementById('videos-link')
+
+if (location.pathname.endsWith('/') || location.pathname.endsWith('/index.html')) {
+    homeLink.classList.add('is-primary')
+}
 
 if (location.pathname.endsWith('/sobre.html')) {
     aboutLink.classList.add('is-primary')
